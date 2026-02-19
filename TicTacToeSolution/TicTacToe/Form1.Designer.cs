482fdefade3e532866f55a3931ea12977d10f2c5
Md  Sadman Azad Mahin <sadmanmahin04@gmail.com>
2026-02-19 23:45:33 +0600

    Add files via upload

---
+++ TicTacToeSolution/TicTacToe/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 77);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(113, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 77);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(194, 61);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(71, 77);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(32, 141);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(71, 68);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(113, 141);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(71, 68);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(194, 143);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(71, 67);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Location = new System.Drawing.Point(12, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(324, 337);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(32, 212);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(71, 67);
+            this.button7.TabIndex = 7;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(113, 212);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(71, 66);
+            this.button8.TabIndex = 8;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(195, 213);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(71, 66);
+            this.button9.TabIndex = 9;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Cyan;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(373, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 31);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "    Turn";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(371, 88);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(133, 96);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Lime;
+            this.textBox1.Location = new System.Drawing.Point(342, 191);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "Player 1";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Lime;
+            this.textBox2.Location = new System.Drawing.Point(457, 191);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Text = "Player 2";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(342, 220);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox2.TabIndex = 12;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(457, 220);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox3.TabIndex = 13;
+            this.richTextBox3.Text = "";
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(402, 339);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(102, 32);
+            this.button10.TabIndex = 14;
+            this.button10.Text = "Play Again";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(402, 378);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(102, 36);
+            this.button11.TabIndex = 15;
+            this.button11.Text = "Reset Game";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+    }
+}
+
---
+++ TicTacToeSolution/TicTacToe/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        string Currentplayer = "X";
+        int count = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button.Text == "")
+            { 
+               button.Text= Currentplayer;
+               button.Enabled = false;
+
+                if (Winner() )
+                {
+                    if (Currentplayer == "X")
+                    {
+                        MessageBox.Show($"{Currentplayer} wins!");
+                        int x = count++;
+                        richTextBox2.Text = Convert.ToString(x);
+                    }
+                    else
+                    {
+                        MessageBox.Show($"{Currentplayer} wins!");
+                        int y = count++;
+                        richTextBox3.Text = Convert.ToString(y);
+                    }
+                        EndGame();
+                    return;
+                }
+
+            }
+
+            SwitchPlayer();
+
+
+
+
+        }
+
+        private void SwitchPlayer()
+        {
+            if (Currentplayer == "X")
+            {
+                Currentplayer = "O";
+            }
+            else
+            {
+                Currentplayer = "X";
+            }
+            if(Currentplayer == "O")
+            {
+                richTextBox1.Text = "Player 2";
+            }
+            else
+            { richTextBox1.Text = "Player 1"; }
+
+        }
+        private bool Winner()
+        {
+            if (button1.Text != "" && button1.Text == button2.Text && button2.Text == button3.Text)
+            {
+                button1.BackColor = Color.Green;
+                button2.BackColor = Color.Green;
+                button3.BackColor = Color.Green;
+                return true;
+            }
+            else if (button4.Text != "" && button4.Text == button5.Text && button5.Text == button6.Text)
+            {
+                button4.BackColor = Color.Green;
+                button5.BackColor = Color.Green;
+                button6.BackColor = Color.Green;
+                return true;
+            }
+            else if (button7.Text != "" && button7.Text == button8.Text && button8.Text == button9.Text)
+            {
+                button7.BackColor = Color.Green;
+                button8.BackColor = Color.Green;
+                button9.BackColor = Color.Green;
+
+                return true;
+            }
+            else if (button1.Text != "" && button1.Text == button4.Text && button4.Text == button7.Text)
+            {
+                button1.BackColor = Color.Green;
+                button4.BackColor = Color.Green;
+                button7.BackColor = Color.Green;
+                return true;
+            }
+            else if (button2.Text != "" && button2.Text == button5.Text && button5.Text == button8.Text)
+            {
+                button2.BackColor = Color.Green;
+                button5.BackColor = Color.Green;
+                button8.BackColor = Color.Green;
+
+                return true;
+            }
+            else if (button3.Text != "" && button3.Text == button6.Text && button6.Text == button9.Text)
+            {
+                button3.BackColor = Color.Green;
+                button6.BackColor = Color.Green;
+                button9.BackColor = Color.Green;
+                return true;
+            }
+            else if (button1.Text != "" && button1.Text == button5.Text && button5.Text == button9.Text)
+            {
+                button1.BackColor = Color.Green;
+                button5.BackColor = Color.Green;
+                button9.BackColor = Color.Green;
+                return true;
+            }
+            else if (button3.Text != "" && button3.Text == button5.Text && button5.Text == button7.Text)
+            {
+                button3.BackColor = Color.Green;
+                button5.BackColor = Color.Green;
+                button7.BackColor = Color.Green;
+                return true;
+            }
+            else
+                return false;
+
+        }
+
+        private void EndGame()
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            button7.Enabled = false;
+            button8.Enabled = false;
+            button9.Enabled = false;
+        }
+
+
+
+
+
+
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+
+            button1.BackColor = Color.White;
+            button2.BackColor = Color.White;
+            button3.BackColor = Color.White;
+            button4.BackColor = Color.White;
+            button5.BackColor = Color.White;
+            button6.BackColor = Color.White;
+            button7.BackColor = Color.White;
+            button8.BackColor = Color.White;
+            button9.BackColor = Color.White;
+            
+
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled =true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+            button8.Enabled = true;
+            button9.Enabled = true;
+            if (Winner())
+            {
+                int count = 0;
+                if (Currentplayer == "X")
+                {
+                    MessageBox.Show($"{Currentplayer} wins!");
+                    int x = count++;
+                    richTextBox2.Text = Convert.ToString(x);
+                }
+                else
+                {
+                    MessageBox.Show($"{Currentplayer} wins!");
+                    int y = count++;
+                    richTextBox3.Text = Convert.ToString(y);
+                }
+                EndGame();
+                return;
+            }
+
+
+
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+
+            richTextBox2.Text = "";
+            richTextBox3.Text = "";
+
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+            button8.Enabled = true;
+            button9.Enabled = true;
+
+        }
+    }
+}
